72653c6b16400ddb88e94648163a3cb07ef611c4
Andrew <awong21@stuy.edu>
2017-12-09 12:58:45 -0500

    added work

---
+++ intro.js
@@ -0,0 +1,20 @@
+var addElement = function(e) {
+    var c = document.getElementById('thelist');
+    c.appendChild(document.createElement('li'));
+};
+var b = document.getElementById('b');
+b.addEventListener('click', addElement);
+
+var changeHeading = function(e) {
+    var heading = document.getElementById('h');
+    heading.innerHTML = this.innerHTML;
+};
+var revertHeading = function(e) {
+    var heading = document.getElementById('h');
+    heading.innerHTML = 'Hello World!';
+};
+var d = document.getElementsByTagName('li');
+for (var i=0;i<d.length;i++){
+    d[i].addEventListener('mouseover', changeHeading);
+    d[i].addEventListener('mouseout', revertHeading);
+};
