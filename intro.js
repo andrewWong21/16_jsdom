4e4a03b90eb002f33cb666642ff9881375417adf
Hyhyme <eluo1@stuy.edu>
2017-12-11 14:28:26 -0500

    added fib functionality

--- intro.js
+++ intro.js
@@ -1,7 +1,10 @@
 var theButton = document.getElementById('b');
+var fibButton= document.getElementById('fibB');
 var heading = document.getElementById('h');
 var list = document.getElementById('thelist');
+var fibList = document.getElementById('fib');
 var listItem = document.getElementsByTagName('li');
+var fibItem = fibList.getElementsByTagName('li');
 
 var addElement = function(e) {
     var newListItem = document.createElement('li');
@@ -24,7 +27,28 @@ var rmItem = function(e) {
     this.remove();
 };
 
+var fib = function(n) {
+    if (n == 0){
+	return 0;
+    }
+
+    if (n == 1){
+	return 1;
+    }
+
+    else {
+	return fib(n - 2) + fib(n - 1);
+    }    
+}
+
+var nextFib = function(e) {
+    var fibNum = document.createElement('li');
+    fibNum.innerHTML = fib(fibItem.length);
+    fibList.appendChild(fibNum);
+}    
+
 theButton.addEventListener('click', addElement);
+fibButton.addEventListener('click', nextFib);
 
 for (var i=0;i<listItem.length;i++){
     listItem[i].addEventListener('mouseover', changeHeading);
