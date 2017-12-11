5337e010af743c1e187bf8542b7460d58130fecc
Hyhyme <eluo1@stuy.edu>
2017-12-10 23:10:36 -0500

    added remove item functionality

--- intro.js
+++ intro.js
@@ -2,6 +2,7 @@ var addElement = function(e) {
     var c = document.getElementById('thelist');
     c.appendChild(document.createElement('li'));
 };
+
 var b = document.getElementById('b');
 b.addEventListener('click', addElement);
 
@@ -9,12 +10,19 @@ var changeHeading = function(e) {
     var heading = document.getElementById('h');
     heading.innerHTML = this.innerHTML;
 };
+
 var revertHeading = function(e) {
     var heading = document.getElementById('h');
     heading.innerHTML = 'Hello World!';
 };
+
+var rmItem = function(e) {    
+    this.remove();
+};
+
 var d = document.getElementsByTagName('li');
 for (var i=0;i<d.length;i++){
     d[i].addEventListener('mouseover', changeHeading);
     d[i].addEventListener('mouseout', revertHeading);
+    d[i].addEventListener('click', rmItem);
 };
