3e4ce544252a37c07d84f52af6423b71e1e49de8
andrewWong21 <awong21@stuy.edu>
2017-12-11 14:37:29 -0500

    added triangle sequence

--- intro.js
+++ intro.js
@@ -1,10 +1,13 @@
 var theButton = document.getElementById('b');
-var fibButton= document.getElementById('fibB');
+var fibButton = document.getElementById('fibB');
+var seqButton = document.getElementById('seqB');
 var heading = document.getElementById('h');
 var list = document.getElementById('thelist');
-var fibList = document.getElementById('fib');
 var listItem = list.getElementsByTagName('li');
+var fibList = document.getElementById('fib');
 var fibItem = fibList.getElementsByTagName('li');
+var seqList = document.getElementById('seq');
+var seqItem = seqList.getElementsByTagName('li');
 
 var addElement = function(e) {
     var newListItem = document.createElement('li');
@@ -45,10 +48,21 @@ var nextFib = function(e) {
     var fibNum = document.createElement('li');
     fibNum.innerHTML = fib(fibItem.length);
     fibList.appendChild(fibNum);
-}    
+};
+
+var seq = function(n) {
+    return (n * (n+1))/2;
+};
+
+var nextSeq = function(e) {
+    var seqNum = document.createElement('li');
+    seqNum.innerHTML = seq(seqItem.length+1);
+    seqList.appendChild(seqNum);
+};
 
 theButton.addEventListener('click', addElement);
 fibButton.addEventListener('click', nextFib);
+seqButton.addEventListener('click', nextSeq);
 
 for (var i=0;i<listItem.length;i++){
     listItem[i].addEventListener('mouseover', changeHeading);
